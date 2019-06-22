8aad95fd5e5d51f3382f3354a1a81011e2c6157c
abcd567a <abcd567@hotmail.com>
2019-06-22 11:49:59 -0400

    Update install_dump1090_mut_1.15.sh

--- install_dump1090_mut_1.15.sh
+++ install_dump1090_mut_1.15.sh
@@ -5,3 +5,10 @@ sudo apt install -y libusb-1.0-0-dev librtlsdr-dev lighttpd
 wget -O ${PWD}/dump1090-mutability_1.15_dev_armhf.deb https://github.com/abcd567a/dump1090/releases/download/v1/dump1090-mutability_1.15_dev_armhf.deb
 cd ${PWD}
 sudo dpkg -i dump1090-mutability_1.15_dev_armhf.deb
+sudo wget -O /etc/udev/rules.d/rtl-sdr.rules "https://raw.githubusercontent.com/osmocom/rtl-sdr/master/rtl-sdr.rules" 
+echo "--------------------"
+echo "INSTALLATION DONE!"
+echo "--------------------"
+echo ""
+echo "PLEASE REBOOT YOUR PI....."
+echo "PLEASE REBOOT YOUR PI....."
