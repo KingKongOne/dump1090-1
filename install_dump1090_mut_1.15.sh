ef08c75e4110e0329d626e0a2f7a3cf6ae6480bc
abcd567a <abcd567@hotmail.com>
2020-03-04 09:47:45 -0500

    Update install_dump1090_mut_1.15.sh

--- install_dump1090_mut_1.15.sh
+++ install_dump1090_mut_1.15.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install -y libusb-1.0-0 librtlsdr0 lighttpd 
 sudo wget -O ${PWD}/dump1090-mutability_1.15_dev_armhf.deb https://github.com/abcd567a/dump1090/releases/download/v1/dump1090-mutability_1.15_dev_armhf.deb
 sudo dpkg -i dump1090-mutability_1.15_dev_armhf.deb
-sudo wget -O /etc/udev/rules.d/rtl-sdr.rules "https://raw.githubusercontent.com/osmocom/rtl-sdr/master/rtl-sdr.rules" 
+sudo wget -O /etc/udev/rules.d/rtl-sdr.rules "https://raw.githubusercontent.com/osmocom/rtl-sdr/f2a9a81/rtl-sdr.rules" 
 sudo dpkg-reconfigure dump1090-mutability
 echo "--------------------"
 echo "INSTALLATION COMPLETED!"
